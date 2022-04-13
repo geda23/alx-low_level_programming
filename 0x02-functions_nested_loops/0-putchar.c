2b6209a1d95c007c197a51985dc0e19cd13d33df
geda23 <gedalove78@gmail.com>
2022-04-14 02:21:59 +0300

    0 put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
+
 /**
  * main - Entry point
  *
- * _putchar: Calls function _putchar in hoberton.h
- *
- * Return: it return a zero
+ * Return: nothing, return void
  */
+
 int main(void)
 {
-_putchar('-');
+_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
@@ -18,5 +18,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
+
 return (0);
 }
